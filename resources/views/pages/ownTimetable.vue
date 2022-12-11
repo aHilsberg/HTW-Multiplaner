78a50fa1d64b2cb18e5842facd4566d6595a6d8d
pauljannasch <s79181@informatik.htw-dresden.de>
2022-12-11 19:03:25 +0100

    feat: add timetable layout

--- resources/views/pages/ownTimetable.vue
+++ resources/views/pages/ownTimetable.vue
@@ -1,5 +1,3 @@
-<script setup lang="ts"></script>
-
 <template layout="default">
     <div>OwnTimetable page</div>
     <button
@@ -8,4 +6,61 @@
     >
         RUN
     </button>
+
+    <div>
+      <FullCalendar :options="options" />
+    </div>
+
 </template>
+
+<script setup lang="ts">
+  import { reactive } from 'vue';
+  import '@fullcalendar/core/vdom';
+  import FullCalendar from '@fullcalendar/vue3';
+  import dayGridPlugin from '@fullcalendar/daygrid';
+  import timeGridPlugin from '@fullcalendar/timegrid';
+  import listPlugin from '@fullcalendar/list';
+  import intercationPlugin from '@fullcalendar/interaction';
+
+  const options = reactive({
+    plugins: [dayGridPlugin, timeGridPlugin, listPlugin, intercationPlugin],
+    initialView: 'timeGridWeek',
+    locale: "de",
+    firstDay: 1,
+
+    slotLabelFormat: {
+      hour12: false,
+      hour: '2-digit',
+      minute: '2-digit'
+    },
+
+    headerToolbar: {
+      left: 'prev,next,today',
+      center: 'title',
+      right: 'timeGridDay,timeGridWeek,dayGridMonth'
+    },
+
+    events: [],
+
+    eventClick: function(info) {
+      alert('Event: ' + info.event.title);
+    }
+  });
+
+  function oddWeek() {
+    var currentDate = new Date();
+    var startDate = new Date(currentDate.getFullYear(), 0, 1);
+    var days = (currentDate - startDate) / (24 * 60 * 60 * 1000);
+    return (Math.floor(days / 7) % 2) == 1;
+  };
+
+  function getEventColor(type) {
+    return ({
+      Vorlesung: 'orange',
+      Praktikum: 'light-blue',
+      Uebung: 'green',
+      Termin: 'blue'
+    }[type]);
+  };
+
+</script>
