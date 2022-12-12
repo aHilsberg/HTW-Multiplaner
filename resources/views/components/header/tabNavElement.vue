e13a5bc4936146b5765e5876298fba22e4d9d6b8
aHilsberg <hilsberg.anton@gmail.com>
2022-12-12 15:33:37 +0100

    feat: use headless tab components

--- resources/views/components/header/tabNavElement.vue
+++ resources/views/components/header/tabNavElement.vue
@@ -1,30 +1,36 @@
 <script setup lang="ts">
-import { XMarkIcon } from "@heroicons/vue/24/outline";
-import { Link } from "@inertiajs/inertia-vue3";
+import {XMarkIcon} from '@heroicons/vue/24/outline'
+import {Link} from '@inertiajs/inertia-vue3'
+import {Tab} from '@headlessui/vue'
 
 const props = defineProps<{
     href: string;
-}>();
+    lastTab: boolean
+}>()
 
 const name = computed(() => {
-    if (props.href === "/") return "Eigener Stundenplan";
+    if (props.href === '/') return 'Eigener Stundenplan'
 
-    return "TODO";
-});
+    return 'TODO'
+})
 
-defineEmits(["close"]);
+defineEmits(['close'])
 </script>
 
 <template>
-    <Link
-        :href="href"
-        as="ul"
-        class="flex items-center gap-2 -ml-2 pl-4 pr-2 py-1 border-t border-r border-gray-300 rounded-tr-xl"
-    >
-        <span class="text-md mr-1">{{ name }}</span>
+    <!-- Use the `selected` state to conditionally style the selected tab. -->
+    <Tab as="template" v-slot="{ selected }">
+        <Link
+            :href="href"
+            as="ul"
+            class="flex items-center gap-2 pl-4 pr-2 py-1 border border-gray-300"
+            :class="{ 'bg-gray-50': !selected, ' rounded-tr-xl': lastTab }"
+        >
+            <span class="text-md mr-1">{{ name }}</span>
 
-        <button v-if="href !== '/'" @click.prevent="$emit('close')">
-            <XMarkIcon class="w-4 h-4 text-gray-600" />
-        </button>
-    </Link>
+            <button v-if="href !== '/'" @click.prevent="$emit('close')">
+                <XMarkIcon class="w-4 h-4 text-gray-600"/>
+            </button>
+        </Link>
+    </Tab>
 </template>
--- resources/views/components/header/tabNavHeader.vue
+++ resources/views/components/header/tabNavHeader.vue
@@ -1,57 +1,61 @@
 <script setup lang="ts">
-import { Link, usePage } from "@inertiajs/inertia-vue3";
+import {Link, usePage} from '@inertiajs/inertia-vue3'
 
-import TabNavElement from "@/views/components/header/tabNavElement.vue";
+import TabNavElement from '@/views/components/header/tabNavElement.vue'
+import {TabGroup, TabList} from '@headlessui/vue'
 
 const validTabsPathNames = [
     // @ts-ignore
-    new URL(route("lookup")).pathname,
-];
+    new URL(route('lookup')).pathname,
+]
 
-const page = usePage();
-const tabs = ref<string[]>([]);
+const page = usePage()
+const tabs = ref<string[]>([])
 
-const loading = ref(false);
+const loading = ref(false)
 
 watchEffect(() => {
-    if (loading.value) return;
+    if (loading.value) return
 
-    const path = page.url.value;
+    const path = page.url.value
     if (!validTabsPathNames.find((validName) => path.startsWith(validName)))
-        return;
+        return
 
-    if (tabs.value.includes(path)) return;
-    console.log({ add: path });
-    tabs.value.push(path);
-});
+    if (tabs.value.includes(path)) return
+    console.log({add: path})
+    tabs.value.push(path)
+})
 
 const removeTab = (index: number) => {
-    const tabsRef = [...tabs.value];
-    console.log({ remove: tabsRef.splice(index, 1) });
-    tabs.value = tabsRef;
-    console.log({ tabsRef });
-    const lastTab = index > 0 ? tabs.value[index - 1] : "/";
+    const tabsRef = [...tabs.value]
+    console.log({remove: tabsRef.splice(index, 1)})
+    tabs.value = tabsRef
+    console.log({tabsRef})
+    const lastTab = index > 0 ? tabs.value[index - 1] : '/'
 
-    loading.value = true;
+    loading.value = true
     Inertia.visit(lastTab, {
         onFinish: () => {
-            loading.value = false;
+            loading.value = false
         },
-    });
-};
+    })
+}
 </script>
 
 <template>
-    <li class="self-end flex items-center">
-        <TabNavElement href="/" />
-        <TabNavElement
-            v-for="(tab, index) in tabs"
-            :key="index"
-            :href="tab"
-            @close="() => removeTab(index)"
-        />
-        <Link as="ul" :href="route('lookup')" :data="{ module: 'B111' }">
-            DEBUG ADD TAB
-        </Link>
-    </li>
+    <TabGroup>
+        <TabList class="self-end flex items-center">
+            <TabNavElement href="/" :last-tab="tabs.length === 0"/>
+            <TabNavElement
+                v-for="(tab, index) in tabs"
+                :key="index"
+                :href="tab"
+                :last-tab="tabs.length === index + 1"
+                @close="() => removeTab(index)"
+            />
+            <Link as="ul" :href="route('lookup')" :data="{ module: 'B111' }">
+                DEBUG ADD TAB
+            </Link>
+        </TabList>
+    </TabGroup>
 </template>
